ce2da75a6e1963a791ae4ba263bfe885952dba79
Gustav Arrhenius <gustav.arrhenius@gmail.com>
2012-12-12 10:48:50 +0100

    RestApi

---
+++ RestApi/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestApi.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Get(int id)
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Update(int id)
+        {
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        public ActionResult LogOn(string email,  string password)
+        {
+            return View();
+        }
+        public ActionResult LogOn(string email)
+        {
+            return View();
+        }
+
+    }
+}
